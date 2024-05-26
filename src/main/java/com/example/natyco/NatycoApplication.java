981b9d0855f1ec8744c367b749e2c2277449e7d8
NatycorRaven <natycoraven@gmail.com>
2024-05-26 21:26:52 +0800

    测试

---
+++ src/main/java/com/example/natyco/NatycoApplication.java
@@ -0,0 +1,13 @@
+package com.example.natyco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NatycoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NatycoApplication.class, args);
+	}
+
+}
